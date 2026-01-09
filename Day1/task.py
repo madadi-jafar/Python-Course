aa1021953a70eca94666f108df6190e65825529d
Jafar Madadi <jafar.rasa76@gmail.com>
2026-01-09 21:55:01 +0430

    feat(day3): add beginner python exercise for Day 1

---
+++ Day1/task.py
@@ -0,0 +1,11 @@
+#Day 1: Variables, Data Types & Basic I/O:
+
+# Task: Ask the user for their name, age, and favorite number.
+# Print a message like: "Hello [name]! You are [age] years old, and your favorite number squared is [number^2]."
+# Use appropriate variable names and data types.
+
+# Starter code:
+name = input("Enter your name: ")
+# TODO: Get age and favorite number from user (convert to correct types)
+# TODO: Calculate square of favorite number
+# TODO: Print formatted message
