467d0a9caf2cf831e4feadc3b23dbc7e228ce263
mahdi <hatamimahdi32@gmail.com>
2026-01-17 22:11:17 -0800

    fourth Day examples commit

---
+++ Day4/examples.py
@@ -0,0 +1,95 @@
+# example.py
+# Python Conditional Statements – Practice Tasks
+
+# Task 1: Single 'if' statement
+# Write a program that checks if a number is positive.
+# If it is, print "The number is positive."
+
+# Your code here:
+
+number = int(input("Enter a number: "))
+if number > 0:
+    print("The number is positive.")
+             
+
+# Task 2: 'if...else' statement
+# Write a program that checks if a number is even or odd.
+# Print "Even" if the number is even, otherwise print "Odd".
+
+# Your code here:
+
+num = int(input("Enter a number: "))
+if number != 0 and number % 2 == 0 :
+    print("Even")
+else :
+    print("Odd")
+
+
+# Task 3: 'if...elif...else' statement
+# Write a program that takes a student's score (0–100) and prints their grade:
+# 90–100: "A"
+# 80–89: "B"
+# 70–79: "C"
+# 60–69: "D"
+# Below 60: "F"
+
+# Your code here:
+grade = float(input("Enter your grade: "))
+if number > 100:
+    print("INVALID SCORE!")
+elif grade >= 90 and grade <= 100 :
+    print("\"A\"")
+elif grade >= 80 and grade < 90 :
+    print("\"B\"")
+elif grade >=70 and grade < 80 :
+    print("\"C\"")
+elif grade >= 60 and grade <70 :
+    print("\"D\"")
+elif grade < 60 and grade >= 0:
+    print("\"F\"")
+else:
+    print(" INVALID SCORE")
+
+
+# Task 4: Nested 'if' statement
+# Write a program that checks if a number is positive.
+# If it is positive, further check if it is even or odd and print the result.
+# If it's not positive, just print "Number is not positive."
+
+# Your code here:
+num = int(input("Enter a number: "))
+if num > 0 :
+    if num % 2 ==0 :
+        print("Even")
+    else:
+        print("Odd")
+else:
+    print("Number is not positive.")
+
+
+
+
+# Task 5: Ternary 'if' (conditional expression)
+# Use a one-line conditional to assign "Adult" to variable 'status' if age >= 18,
+# otherwise assign "Minor". Assume age = 20. Print 'status'.
+
+# Your code here:
+age = 20
+status = "Adult" if age >= 18 else "Minor"
+
+
+# Task 6: Logical operators (and, or, not)
+# Write a program that checks if a person is eligible to vote:
+# - Must be at least 18 years old AND
+# - Must be a citizen (represented by a boolean variable 'is_citizen')
+# Print "Eligible to vote" if both conditions are True, otherwise print "Not eligible".
+
+# Test with: age = 17, is_citizen = True
+
+# Your code here:
+age = 17
+is_citizen = True
+if age >= 18 and is_citizen == True:
+    print("Eligible to vote")
+else:
+    print("Not eligible")
