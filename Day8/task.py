21a90fbd5a2496bc5d5ae96901af380c2b0530eb
Jafar Madadi <jafar.rasa76@gmail.com>
2026-01-20 21:16:41 +0430

    add(task): day8

---
+++ Day8/task.py
@@ -0,0 +1,13 @@
+# Day 8: For Loops & Lists
+
+# Task: Ask the user for 5 numbers and store them in a list.
+# Then print:
+# - The list
+# - Sum of numbers
+# - Average (rounded to 2 decimals)
+# - Largest number
+
+# Starter code:
+numbers = []
+# TODO: Use a for loop to collect 5 numbers
+# TODO: Calculate and print required stats
