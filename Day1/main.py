d660cf2f96b3c1544b9163b09af5d6222727e5f7
shahram <shahramhatami595@gmail.com>
2026-01-20 23:12:48 +0430

    first day task

---
+++ Day1/main.py
@@ -0,0 +1,9 @@
+#Day 1: Variables, Data Types & Basic input/output:
+name = input("enter your name:")
+
+# ask age and favorite numer
+age = int(input("enter your age:"))
+favoriteNumber = float(input("enter your favorite number:"))
+squareOfFavoriteNumber = favoriteNumber**2
+# print output
+print("hello",name,"you are",age,"years old, and your favorite number squared is",squareOfFavoriteNumber)
