bfb689db686dc8aae355734ebf23a696651fce98
shahram <shahramhatami595@gmail.com>
2026-01-22 11:39:37 +0430

    my example commit

--- Day4/examples.py
+++ Day4/examples.py
@@ -6,17 +6,22 @@
 # If it is, print "The number is positive."
 
 # Your code here:
-
-
-
+number =float( input("Enter a number for cheacking:"))
+if number > 0:
+    print("The",number,"is possitive")
 
 # Task 2: 'if...else' statement
 # Write a program that checks if a number is even or odd.
 # Print "Even" if the number is even, otherwise print "Odd".
 
 # Your code here:
-
-
+number = float(input("Enter a number for cheackinfg:"))
+if number == 0 :
+    print("The number is zero")
+elif number % 2 == 0:
+    print("even")
+else:
+    print("odd")
 
 
 # Task 3: 'if...elif...else' statement
@@ -28,8 +33,17 @@
 # Below 60: "F"
 
 # Your code here:
-
-
+number = int(input("Please enter your score:"))
+if number >= 90 and number <= 100:
+    print("A")
+elif number >= 80 and number <= 89:
+    print("B")
+elif number >= 70 and number <= 79:
+    print("C")
+elif number >= 60 and number <= 69:
+    print("D")
+else:
+    print("F")
 
 
 # Task 4: Nested 'if' statement
@@ -38,6 +52,16 @@
 # If it's not positive, just print "Number is not positive."
 
 # Your code here:
+number = int(input("Enter a number cheacking:"))
+if number > 0:
+    if number % 2 == 0:
+        print("Even")
+    elif number % 2 != 0:
+        print("Odd") 
+    else:
+        print("Zero")
+else:
+    print("The number is negative")               
 
 
 
@@ -47,7 +71,9 @@
 # otherwise assign "Minor". Assume age = 20. Print 'status'.
 
 # Your code here:
-
+age = 20
+status = "Adult" if age >= 18 else "Minor"
+print("status")
 
 
 
@@ -60,4 +86,9 @@
 # Test with: age = 17, is_citizen = True
 
 # Your code here:
-
+age =int(input("Please enter your age:"))
+is_citizen = input("are you a citizen?(yes/no) \n ").lower()
+if age>= 18 and is_citizen == 'yes':
+    print("You are eligible for voting")
+else:
+    print("You are not eligble for voting")   
