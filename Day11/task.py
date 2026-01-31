d945034c709c90321c4308de0551094127a33f81
Jafar Madadi <jafar.rasa76@gmail.com>
2026-01-31 19:18:35 +0430

    add(task):Day11

---
+++ Day11/task.py
@@ -0,0 +1,15 @@
+# Day 11: Positional vs Keyword Arguments
+
+# Task: Define a function `greet(first, last, greeting="Hello")` that returns a string:
+# "[greeting], [first] [last]!"
+# Call it 3 ways:
+# 1. All positional
+# 2. Mix of positional and keyword
+# 3. All keyword
+# Print each result.
+
+# Starter code:
+def greet(first, last, greeting="Hello"):
+    # TODO: Return formatted string
+
+# TODO: Make 3 function calls and print results
