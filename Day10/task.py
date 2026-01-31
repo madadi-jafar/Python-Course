4cca8d82ba82e445936993bf1da6b7cc7ce7d735
Jafar Madadi <jafar.rasa76@gmail.com>
2026-01-31 19:17:02 +0430

    add(task):Day10

---
+++ Day10/task.py
@@ -0,0 +1,16 @@
+# Day 10: Dictionaries & Nested Collections
+
+
+
+# Task: Create a dictionary `student` with keys: "name", "grades" (a list of 3 scores).
+# Calculate average grade and add a new key "average".
+# Print the full dictionary and a message like "[Name]'s average is X".
+
+# Starter code:
+student = {
+    "name": input("Enter student name: "),
+    "grades": []
+}
+# TODO: Collect 3 grades using a loop
+# TODO: Compute average and add to dictionary
+# TODO: Print result
