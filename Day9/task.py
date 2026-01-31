6c8c25b7f18bf529c891c02a1566ddbefb9dfc74
Jafar Madadi <jafar.rasa76@gmail.com>
2026-01-31 19:14:28 +0430

    add(task): Day9

---
+++ Day9/task.py
@@ -0,0 +1,12 @@
+# Day 9: While Loops & Input Validation
+
+# Task: Keep asking the user to enter a password until they type "secret123".
+# After correct password, print "Access granted!".
+# Also count how many attempts were made.
+
+# Starter code:
+password = ""
+attempts = 0
+# TODO: Use while loop to check password
+# TODO: Increment attempts each time
+# TODO: Print success message and number of attempts
