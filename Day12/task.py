42aa1ea2117a1bc8c2c018decec23a31c9013a89
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-02 17:32:59 +0430

    add(Task): Day 12 of Python Basics

---
+++ Day12/task.py
@@ -0,0 +1,14 @@
+# Day 12: Scope & Global/Local Variable
+
+# Task: Define a global variable `counter = 0`.
+# Write a function `increment()` that uses the `global` keyword to increase `counter` by 1.
+# Call the function 3 times, then print the final value of `counter`.
+
+# Starter code:
+counter = 0
+
+def increment():
+    # TODO: Use global keyword to modify counter
+
+# TODO: Call increment() 3 times
+# TODO: Print counter
