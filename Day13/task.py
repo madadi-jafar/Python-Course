e6e8bca496a32e058f53d7eafe14baae83202cf5
Jafar Madadi <jafar.rasa76@gmail.com>
2026-02-04 17:59:10 +0430

    add(task):Day 13 of Python Basic

---
+++ Day13/task.py
@@ -0,0 +1,13 @@
+# Day 13: Debugging & Code Blocks
+
+# Task: The code below has 3 bugs (indentation, logic, variable name).
+# Fix it so it prints even numbers from 2 to 10.
+
+# Starter code (with intentional bugs):
+for i in range(1, 11)
+if i % 2 = 0
+print(i)
+
+# TODO: Correct the syntax and logic errors
+
+
